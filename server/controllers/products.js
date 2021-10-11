45888c6cd72b5ad9a351a25e6df948fdaec5eaa9
A-Mosher <adam.l.mosher@gmail.com>
2021-10-11 16:34:39 -0400

    added updateProduct route in controllers and routes.

--- server/controllers/products.js
+++ server/controllers/products.js
@@ -1,25 +1,40 @@
+import mongoose from "mongoose";
 import ProductInfo from "../models/productInfo.js";
 
 export const getProducts = async (req, res) => {
-    try {
-        const allProducts =  await ProductInfo.find();
+  try {
+    const allProducts = await ProductInfo.find();
 
-        res.status(200).json(allProducts);
-    } catch (error) {
-        res.status(404).json({ message: error.message });
-    }
-}
+    res.status(200).json(allProducts);
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+};
 
 export const createProduct = async (req, res) => {
-    const product = req.body;
+  const product = req.body;
 
-    const newProduct = ProductInfo(product)
+  const newProduct = ProductInfo(product);
 
-    try {
-        await newProduct.save();
+  try {
+    await newProduct.save();
 
-        res.status(201).json(newProduct);
-    } catch (error) {
-        res.status(409).json({ message: error.message });
-    }
-}
+    res.status(201).json(newProduct);
+  } catch (error) {
+    res.status(409).json({ message: error.message });
+  }
+};
+
+export const updateProduct = async (req, res) => {
+  const { id: _id } = req.params;
+  const product = req.body;
+
+  if (!mongoose.Types.ObjectId.isValid(_id))
+    return res.status(404).send("No product with that id");
+
+  const updatedProduct = await ProductInfo.findByIdAndUpdate(_id, product, {
+    new: true,
+  });
+
+  res.json(updatedProduct);
+};
